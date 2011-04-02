e287bfdc95aba0c2b862aceca542bd80746432e7
Obi Akubue <obioraakubue@yahoo.com>
2011-04-02 16:55:51 -0400

    Create Database action

---
+++ lib/leanback.rb
@@ -0,0 +1,29 @@
+require 'rest_client'
+require 'yajl'
+
+module Couchdb
+
+  def self.create(database_name)
+       set_url
+       begin
+         response = RestClient.put 'http://' + @url + ':' + @port + '/' + database_name, {:content_type => :json}
+         hash = Yajl::Parser.parse(response.to_str)
+       rescue => e
+         hash = Yajl::Parser.parse(e.response.to_s)
+       end
+  end
+
+
+
+ class << self
+       attr_accessor :url 
+       attr_accessor :port 
+     
+     def set_url()
+      if @url == nil && port == nil
+         @url = 'localhost'
+         @port = '5984'
+      end 
+     end
+  end
+end

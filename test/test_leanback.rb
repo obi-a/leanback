d6e6bf0b5b3dc014da72d3f7e4e9c8616663c978
Obi Akubue <obioraakubue@yahoo.com>
2011-04-02 18:35:39 -0400

    Added 2 test cases for database creation

--- test/test_leanback.rb
+++ test/test_leanback.rb
@@ -1,7 +1,27 @@
-require 'helper'
+path = File.expand_path(File.dirname(__FILE__))
+
+require path + "/helper.rb"
 
 class TestLeanback < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  #should "probably rename this file and start testing for real" do
+   # flunk "hey buddy, you should probably rename this file and start testing for real"
+  #end
+
+
+  should "create a database if it doesn't already exist" do
+ 	hash = Couchdb.create 'more_books'
+	puts hash.inspect
   end
+
+  should "return error message illegal database name" do
+ 	hash = Couchdb.create 'more books'
+	puts hash.inspect
+  end
+ 
+ 
 end
+
+
+
+
+

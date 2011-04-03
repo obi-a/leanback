2e14ad7ba36097cb31eda62d6ae7d3f96dd4a549
Obi Akubue <obioraakubue@yahoo.com>
2011-04-02 23:29:56 -0400

    Added more DB actions

--- lib/leanback.rb
+++ lib/leanback.rb
@@ -1,6 +1,73 @@
 require 'rest_client'
 require 'yajl'
 
+module Document
+  
+  #create a document 
+  def self.create( doc)  
+      
+      db_name =  doc[:database]
+      doc_id = doc[:doc_id]
+      data = doc[:data]
+      
+      json_data = Yajl::Encoder.encode(data)
+      
+      set_url
+
+      begin
+         response = RestClient.put 'http://' + @url + ':' + @port + '/' + URI.escape(db_name) + '/' + URI.escape(doc_id),json_data, {:content_type => :json, :accept => :json}
+         hash = Yajl::Parser.parse(response.to_str)
+       rescue => e
+         hash = Yajl::Parser.parse(e.response.to_s)
+       end
+  end
+
+  #edit a document
+  def self.edit(doc)
+      db_name = doc[:database]
+      doc_id = doc[:doc_id]
+      data = doc[:data]
+     
+      json_data = Yajl::Encoder.encode(data)
+      
+      set_url
+
+      begin
+        response = RestClient.put 'http://' + @url + ':' + @port + '/' + URI.escape(db_name) + '/' + URI.escape(doc_id), json_data, {:content_type => :json, :accept => :json}
+         hash = Yajl::Parser.parse(response.to_str)
+       rescue => e
+         hash = Yajl::Parser.parse(e.response.to_s)
+      end
+  end
+
+ #delete a doc
+ def self.delete(doc)
+   db_name = doc[:database]
+   doc_id = doc[:doc_id]
+   rev = doc[:rev]
+   set_url
+   begin 
+    response = RestClient.delete 'http://' + @url + ':' + @port + '/' + URI.escape(db_name)  + '/' + URI.escape(doc_id) + '?rev=' + rev, {:content_type => :json}
+      hash = Yajl::Parser.parse(response.to_str)
+    rescue => e
+     hash = Yajl::Parser.parse(e.response.to_s)
+    end
+   
+ end
+
+   class << self
+       attr_accessor :url 
+       attr_accessor :port 
+     
+     def set_url()
+      if @url == nil && port == nil
+         @url = 'localhost'
+         @port = '5984'
+      end 
+     end
+  end
+end
+
 module Couchdb
   #create a database if one with the same name doesn't already exist
   def self.create(database_name)
@@ -12,6 +79,18 @@ module Couchdb
          hash = Yajl::Parser.parse(e.response.to_s)
        end
   end
+  
+
+ #delete a database
+ def self.delete(database_name)
+      set_url
+       begin
+         response = RestClient.delete 'http://' + @url + ':' + @port + '/' + URI.escape(database_name), {:content_type => :json}
+         hash = Yajl::Parser.parse(response.to_str)
+       rescue => e
+         hash = Yajl::Parser.parse(e.response.to_s)
+       end 
+ end
 
  #return a list of all databases
  def self.all_dbs
@@ -23,8 +102,6 @@ module Couchdb
            raise e
        end
  end
-
-
 
 
  class << self
--- test/test_leanback.rb
+++ test/test_leanback.rb
@@ -18,16 +18,38 @@ class TestLeanback < Test::Unit::TestCase
 #	puts hash.inspect
  # end
 
-  should "return a display a list of all databases" do
-      databases = Couchdb.all_dbs
-        databases.each do |db_name| 
-           puts db_name
-        end
+  #should "return a display a list of all databases" do
+   #   databases = Couchdb.all_dbs
+    #    databases.each do |db_name| 
+     #      puts db_name
+      #  end
+   #end
+
+  # should "delete a database" do 
+   #    hash = Couchdb.delete 'more_books'
+    #   puts hash.inspect
+   #end
+
+  should "create a document" do 
+         data = {:firstname => 'Mary', :lastname =>'smith', :phone => '212-234-1234',:email =>'john@mail.com'}
+         doc = {:database => 'contacts', :doc_id => 'Mary', :data => data}
+         hash = Document.create doc
+         puts hash.inspect    
+  end
+
+ #should "edit a document" do 
+  #       data = {:firstname => 'john', :lastname =>'smith', :email => 'john@mail.com',:gender=>'male', :_rev=>'2-57e1f041838e19d91191e391970d16ce'}
+   #      doc = {:database => 'contacts', :doc_id => 'john', :data => data}
+    #     hash = Document.edit doc
+     #    puts hash.inspect    
+  #end
+
+   should "delete a document" do 
+         doc = {:database => 'contacts', :doc_id => 'john', :rev => '3-be02e80490f8e9e610d9a9e33d752316'}
+         Document.delete doc
+         puts hash.inspect
    end
-
   
-   
-
 end
 
 
